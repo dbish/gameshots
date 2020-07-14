da6ff496916d48e63efca1490f03c4417e788582
dbish <diamondbishop@gmail.com>
2020-07-14 07:51:46 +0000

    favicon updated

--- application.py
+++ application.py
@@ -496,7 +496,7 @@ def unfollowUser():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return app.send_static_file('favicon.ico')
 
 @app.route('/')
 def home():
