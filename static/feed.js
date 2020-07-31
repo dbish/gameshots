f9a359975d7ec1932dd9285b1f9dc26ac8e2776e
dbish <diamondbishop@gmail.com>
2020-07-31 04:36:20 +0000

    split shared functions out for post page and feed so extra functions like infinite scroll related ones aren't loaded up on post page

--- static/feed.js
+++ static/feed.js
@@ -1,98 +1,6 @@
 var earliestSeen;
 var postIDs;
-var username; 
 var following;
-
-var colors  = ['Aqua', 'Aquamarine', 'Blue', 'BlueViolet', 'Chartreuse', 'Coral', 'CornflowerBlue', 'Crimson',
-	'Cyan', 'DarkMagenta', 'DarkOrange', 'Fuchsia', 'ForestGreen', 'Gold', 'GreenYellow',
-	'HotPink', 'Lavender', 'LawnGreen', 'Lime', 'Magenta', 'MediumSpringGreen', 'Red', 'RoyalBlue', 'Yellow']
-
-function getColor(name){
-	var value = name.charCodeAt(0)*name.length;
-	return colors[value%colors.length];
-}
-
-function postComment(postID, user){
-	var commentBox = $('#'+postID).find('.commentbox');
-	var text = commentBox.val();
-	commentBox.val('');
-	$.post('/postComment',{
-		'text':text,
-		'postID':postID
-	}).done(function(response){
-		var commentID = response; 
-		addComment(postID, text, user, commentID);
-	}).fail(function(){
-		console.log('could not post comment');
-	});
-}
-
-function addComment(postID, text, user, commentID){
-	var commentsList = $('#'+postID).find('.commentsList');
-	var newLI = '<li class="mycomment"><button type="button" class="btn btn-sm btn-danger" onclick="deleteComment(\''+commentID+'\', this)">-</button>';
-	newLI +='<a href="/gamer/'+user+'" style="color:'+getColor(user)+'">'+user+'</a>: '+text;
-	newLI += '</li>';
-	commentsList.append(newLI);
-
-}
-
-function deleteComment(commentID, e){
-	$.post('/deleteComment', {
-		'commentID':commentID
-	}).done(function(response){
-		$(e).parent().remove();
-	}).fail(function(){
-		console.log('failed to delete');
-	});
-}
-
-
-function deletePost(postID){
-
-	$.post('/deletePost', {
-		'postID':postID
-	}).done(function(response){
-
-	}).fail(function(){
-		console.log('failed to delete');
-	});
-}
-
-function downvotePost(postID){
-	var coins = $('#'+postID).find('.coins');
-	var downvoteButton = $('#'+postID).find('.downvoteButton');
-	var upvoteButton = $('#'+postID).find('.upvoteButton');
-
-	$.post('/downvote', {
-		'postID':postID
-	}).done(function(response){
-		coins.text(parseInt(coins.text())-1);
-		downvoteButton.hide();
-		upvoteButton.show();
-		console.log('downvoted!');
-
-	}).fail(function(){
-		console.log('failed to downvote');
-	});
-}
-
-function upvotePost(postID){
-	var coins = $('#'+postID).find('.coins');
-	var downvoteButton = $('#'+postID).find('.downvoteButton');
-	var upvoteButton = $('#'+postID).find('.upvoteButton');
-	
-
-	$.post('/upvote', {
-		'postID':postID
-	}).done(function(response){
-		coins.text(parseInt(coins.text())+1);
-		upvoteButton.hide();
-		downvoteButton.show();
-		console.log('upvoted!');
-	}).fail(function(){
-		console.log('failed to vote');
-	});
-}
 
 function toggleGame(game, e){
 	$("."+game).each(function(){
@@ -145,11 +53,11 @@ function addNewPost(info){
 	body.addClass('card-body');
 	
 	if (voted){
-		body.append('<button class="btn upvoteButton" onclick="upvotePost('+info[6]+')" style="display:none">+1</button>');
-		body.append('<button class="btn downvoteButton" onclick="downvotePost('+info[6]+')">-1</button>');
+		body.append('<button class="btn upvoteButton" onclick="upvotePost(\''+info[6]+'\')" style="display:none">+1</button>');
+		body.append('<button class="btn downvoteButton" onclick="downvotePost(\''+info[6]+'\')">-1</button>');
 	}else{
-		body.append('<button class="btn upvoteButton" onclick="upvotePost('+info[6]+')">+1</button>');
-		body.append('<button class="btn downvoteButton" onclick="downvotePost('+info[6]+')" style="display:none">-1</button>');
+		body.append('<button class="btn upvoteButton" onclick="upvotePost(\''+info[6]+'\')">+1</button>');
+		body.append('<button class="btn downvoteButton" onclick="downvotePost(\''+info[6]+'\')" style="display:none">-1</button>');
 	}
 	body.append('<button class="btn more"><i class="fa fa-ellipsis-h"></i></button>');
 	
@@ -187,9 +95,9 @@ $(window).scroll(function (){
 		console.log('getting more posts');
         $.ajax({
             url: '/scrollFeed?before='+earliestSeen,
-            async: true,            // by default, it's async, but...
-            dataType: 'json',       // or the dataType you are working with
-            timeout: 10000,          // IMPORTANT! this is a 10 seconds timeout
+            async: true,            
+            dataType: 'json',       
+            timeout: 10000,         
             cache: false
 
         }).done(function (data, textStatus, jqXHR) {
---
+++ static/post.js
@@ -0,0 +1,106 @@
+var username;
+
+var colors  = ['Aqua', 'Aquamarine', 'Blue', 'BlueViolet', 'Chartreuse', 'Coral', 'CornflowerBlue', 'Crimson',
+        'Cyan', 'DarkMagenta', 'DarkOrange', 'Fuchsia', 'ForestGreen', 'Gold', 'GreenYellow',
+        'HotPink', 'Lavender', 'LawnGreen', 'Lime', 'Magenta', 'MediumSpringGreen', 'Red', 'RoyalBlue', 'Yellow']
+
+function getColor(name){
+        var value = name.charCodeAt(0)*name.length;
+        return colors[value%colors.length];
+}
+
+function postComment(postID, user){
+        var commentBox = $('#'+postID).find('.commentbox');
+        var text = commentBox.val();
+        commentBox.val('');
+        $.post('/postComment',{
+                'text':text,
+                'postID':postID
+        }).done(function(response){
+                var commentID = response;
+                addComment(postID, text, user, commentID);
+        }).fail(function(){
+                console.log('could not post comment');
+        });
+}
+
+function addComment(postID, text, user, commentID){
+        var commentsList = $('#'+postID).find('.commentsList');
+        var newLI = '<li class="mycomment"><button type="button" class="btn btn-sm btn-danger" onclick="deleteComment(\''+commentID+'\', this)">-</button>';
+        newLI +='<a href="/gamer/'+user+'" style="color:'+getColor(user)+'">'+user+'</a>: '+text;
+        newLI += '</li>';
+        commentsList.append(newLI);
+
+}
+
+function deleteComment(commentID, e){
+        $.post('/deleteComment', {
+                'commentID':commentID
+        }).done(function(response){
+                $(e).parent().remove();
+        }).fail(function(){
+                console.log('failed to delete');
+        });
+}
+
+
+function deletePost(postID){
+
+        $.post('/deletePost', {
+                'postID':postID
+        }).done(function(response){
+
+        }).fail(function(){
+                console.log('failed to delete');
+        });
+}
+
+function downvotePost(postID){
+        var coins = $('#'+postID).find('.coins');
+        var downvoteButton = $('#'+postID).find('.downvoteButton');
+        var upvoteButton = $('#'+postID).find('.upvoteButton');
+
+        $.post('/downvote', {
+                'postID':postID
+        }).done(function(response){
+                coins.text(parseInt(coins.text())-1);
+                downvoteButton.hide();
+                upvoteButton.show();
+                console.log('downvoted!');
+
+        }).fail(function(){
+                console.log('failed to downvote');
+        });
+}
+
+function upvotePost(postID){
+        var coins = $('#'+postID).find('.coins');
+        var downvoteButton = $('#'+postID).find('.downvoteButton');
+        var upvoteButton = $('#'+postID).find('.upvoteButton');
+
+
+        $.post('/upvote', {
+                'postID':postID
+        }).done(function(response){
+                coins.text(parseInt(coins.text())+1);
+                upvoteButton.hide();
+                downvoteButton.show();
+                console.log('upvoted!');
+        }).fail(function(){
+                console.log('failed to vote');
+        });
+}
+
+function toggleGame(game, e){
+        $("."+game).each(function(){
+                if (e.checked){
+                        $(this).show();
+                }
+                else {
+                        $(this).hide();
+                }
+        });
+}
+
+
+
