771a2f144ecfd0fe047ae426d02ff49155e04c19
Tim Misiak <tmisiak@microsoft.com>
2020-08-09 17:58:09 -0700

    Add game name parsing for client

--- client/BrowseItemViewModel.cs
+++ client/BrowseItemViewModel.cs
@@ -1,34 +1,51 @@
 ﻿using Microsoft.WindowsAPICodePack.Shell;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace GGShot
 {
     class BrowseItemViewModel
     {
-        private string m_fileName;
+        private string m_localPath;
 
-        public BrowseItemViewModel(string fileName)
+        public BrowseItemViewModel(string localPath)
         {
-            m_fileName = fileName;
+            m_localPath = localPath;
+            string filename = Path.GetFileNameWithoutExtension(localPath);
+            Regex nameMatcher = new Regex(@"([\w _]+) \d+[-_]\d+[-_]\d+ \d+[-_]\d+[-_]\d+");
+
+            var match = nameMatcher.Match(filename);
+            if (match.Success)
+            {
+                // Most likely character to get replaced in most game names is colon... May need
+                // to special case otherwise.
+                GameName = match.Groups[1].Value.Replace('_', ':');
+            }
+            else
+            {
+                GameName = "Unknown";
+            }
         }
 
-        public string FileName => m_fileName;
+        public string LocalPath => m_localPath;
+        public string GameName { get; private set; }
 
         public object ItemSource
         {
             get
             {
-                if (m_fileName.EndsWith(".png", StringComparison.OrdinalIgnoreCase))
+                if (m_localPath.EndsWith(".png", StringComparison.OrdinalIgnoreCase))
                 {
-                    return new Uri(m_fileName);
+                    return new Uri(m_localPath);
                 }
                 else
                 {
-                    var sf = ShellFile.FromFilePath(m_fileName);
+                    var sf = ShellFile.FromFilePath(m_localPath);
                     return sf.Thumbnail.ExtraLargeBitmapSource;
                 }
             }
